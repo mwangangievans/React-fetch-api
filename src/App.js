643a9e34813275e1087a46ecb1b069d261e8e3d3
MuindeMwangangi <muindemwangangi2@gmail.com>
2021-05-17 13:52:10 -0700

    Text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import List from './Components/List';
+import withListLoading from './Components/withListLoading';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const ListLoading = withListLoading(List);
+    const [appState, setAppState] = useState({
+        loading: false,
+        repos: null,
+    });
 
+    useEffect(() => {
+        setAppState({ loading: true });
+        const apiUrl = `https://api.github.com/users/mwangangievans/repos`;
+        fetch(apiUrl)
+            .then((res) => res.json())
+            .then((repos) => {
+                setAppState({ loading: false, repos: repos });
+            });
+    }, [setAppState]);
+    return ( <
+        div className = 'App' >
+        <
+        div className = 'container' >
+        <
+        h1 > My Repositories < /h1> < /
+        div > <
+        div className = 'repo-container' >
+        <
+        ListLoading isLoading = { appState.loading }
+        repos = { appState.repos }
+        /> < /
+        div > <
+        footer >
+        <
+        div className = 'footer' >
+
+        with love by evans mwangangi <
+        /div> < /
+        footer > <
+        /div>
+    );
+}
 export default App;
